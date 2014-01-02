bbd089c4c4d38a7e9f45cb282383026a1e99b0a9
jreback <jeff@reback.net>
2014-01-02 06:42:04 -0500

    COMPAT: allow string concats to datetimeindex only (int/float are invalid)

--- pandas/tseries/index.py
+++ pandas/tseries/index.py
@@ -1546,9 +1546,11 @@ class DatetimeIndex(Int64Index):
                                     self[loc:].asi8))
             return DatetimeIndex(new_index, freq='infer')
         except (AttributeError, TypeError):
-            # fall back to object index
-            return self.asobject.insert(loc, item)
 
+            # fall back to object index
+            if isinstance(item,compat.string_types):
+                return self.asobject.insert(loc, item)
+            raise TypeError("cannot insert DatetimeIndex with incompatible label")
 
     def delete(self, loc):
         """
@@ -1589,7 +1591,7 @@ class DatetimeIndex(Int64Index):
     def tz_localize(self, tz, infer_dst=False):
         """
         Localize tz-naive DatetimeIndex to given time zone (using pytz)
-       
+
         Parameters
         ----------
         tz : string or pytz.timezone
