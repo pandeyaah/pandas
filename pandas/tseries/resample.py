4ccafbfe086b1bb2034d552af6b52a4721553bce
Safia Abdalla <seabdalla@gmail.com>
2015-07-10 22:13:30 -0700

    Changed if-statement to be lower-bound inclusive

--- pandas/tseries/resample.py
+++ pandas/tseries/resample.py
@@ -325,7 +325,7 @@ class TimeGrouper(Grouper):
 
         min_base = (type(self.freq))(0)
         base_freq = (type(self.freq))(self.base)
-        if (base_freq < min_base) or (base_freq > self.freq):
+        if (base_freq <= min_base) or (base_freq > self.freq):
             raise Exception("Value of self.base must be between 0 and {freq}, "
                     "got {base} instead".format(freq=self.freq.n, base=self.base))
 
